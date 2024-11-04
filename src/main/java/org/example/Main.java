c6602907dd0ed4c6536cf6d3122bcc6ba7d48c74
tiagoafonseca <tiago.a.afonseca@gmail.com>
2024-11-04 17:23:45 +0000

    interface html

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int a = 1;
-        if(a == 1) {
-            System.out.println("1");
-        }
     }
 }
