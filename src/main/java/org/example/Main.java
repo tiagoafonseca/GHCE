fb0f053ffe1affb56f96b3a24a4c07db175bdb6d
tiagoafonseca <tiago.a.afonseca@gmail.com>
2024-11-04 12:58:16 +0000

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 1;
+        if(a == 1) {
+            System.out.println("1");
+        }
+    }
+}
